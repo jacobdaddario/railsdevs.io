75ec882cdd373f3a2a578ab45ed864dab6974497
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-03 22:25:31 +0800

    Delete config directory

--- config/initializers/constants.rb
+++
@@ -1,1 +0,0 @@
-SECONDS_IN_AN_HOUR = 60 * 60
--- config/initializers/form_errors.rb
+++
@@ -1,4 +0,0 @@
-# Add error span to form fields with errors
-ActionView::Base.field_error_proc = proc do |html_tag, instance|
-  FieldErrorTagBuilder.new(html_tag, instance).error_field
-end
